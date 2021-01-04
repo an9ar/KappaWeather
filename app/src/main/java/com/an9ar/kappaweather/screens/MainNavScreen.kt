14ed38791b137b407d3637a7f32a60842af7a687
Dmitriy <63235125+an9ar@users.noreply.github.com>
2021-01-04 14:35:13 +0300

    Added bottom navigation bar

--- app/src/main/java/com/an9ar/kappaweather/screens/MainNavScreen.kt
+++ app/src/main/java/com/an9ar/kappaweather/screens/MainNavScreen.kt
@@ -1,7 +1,10 @@
 package com.an9ar.kappaweather.screens
 
-import androidx.compose.material.Surface
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.*
 import com.an9ar.kappaweather.theme.AppTheme
 import com.an9ar.kappaweather.theme.KappaWeatherTheme
 
@@ -9,7 +12,61 @@ import com.an9ar.kappaweather.theme.KappaWeatherTheme
 fun MainNavScreen() {
     KappaWeatherTheme {
         Surface(color = AppTheme.colors.background) {
+            val navHostController = rememberNavController()
+            val navItems = listOf(
+                Screens.WeatherScreen,
+                Screens.LocationScreen,
+                Screens.SettingsScreen
+            )
+            Scaffold(
+                bottomBar = {
+                    KappaWeatherBottomNavigation(
+                        navHostController = navHostController,
+                        navItems = navItems
+                    )
+                }
+            ) {
+                NavHost(navController = navHostController, startDestination = Screens.WeatherScreen.routeName) {
+                    composable(Screens.WeatherScreen.routeName) {
 
+                    }
+                    composable(Screens.LocationScreen.routeName) {
+
+                    }
+                    composable(Screens.SettingsScreen.routeName) {
+
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun KappaWeatherBottomNavigation(
+    navHostController: NavHostController,
+    navItems: List<Screens>
+) {
+    BottomNavigation {
+        val navBackStackEntry by navHostController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.arguments?.getString(KEY_ROUTE)
+        navItems.forEach { screen ->
+            BottomNavigationItem(
+                icon = {
+                    Icon(
+                        imageVector = screen.screenIcon,
+                        tint = AppTheme.colors.text
+                    )
+                },
+                label = { Text(text = screen.screenName) },
+                selected = currentRoute == screen.routeName,
+                onClick = {
+                    navHostController.navigate(screen.routeName) {
+                        popUpTo = navHostController.graph.startDestination
+                        launchSingleTop = true
+                    }
+                }
+            )
         }
     }
 }
--- app/src/main/java/com/an9ar/kappaweather/screens/Screens.kt
+++ app/src/main/java/com/an9ar/kappaweather/screens/Screens.kt
@@ -1,7 +1,13 @@
 package com.an9ar.kappaweather.screens
 
-sealed class Screens(name: String) {
-    object WeatherScreen : Screens(name = "WeatherScreen")
-    object LocationScreen : Screens(name = "WeatherScreen")
-    object SettingsScreen : Screens(name = "WeatherScreen")
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.LocationOn
+import androidx.compose.material.icons.outlined.Refresh
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed class Screens(val routeName: String, val screenName: String, val screenIcon: ImageVector) {
+    object WeatherScreen : Screens(routeName = "WeatherScreen", "Weather", Icons.Outlined.Refresh)
+    object LocationScreen : Screens(routeName = "LocationScreen", "Location", Icons.Outlined.LocationOn)
+    object SettingsScreen : Screens(routeName = "SettingsScreen", "Settings", Icons.Outlined.Settings)
 }
