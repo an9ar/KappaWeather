91bc0e0167cb78f31bbed32daa06b933d566ed6b
Dmitriy <63235125+an9ar@users.noreply.github.com>
2021-01-08 21:20:09 +0300

    Added app versions section on setting screen

--- app/src/main/java/com/an9ar/kappaweather/screens/SettingsScreen.kt
+++ app/src/main/java/com/an9ar/kappaweather/screens/SettingsScreen.kt
@@ -1,15 +1,39 @@
 package com.an9ar.kappaweather.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
+import com.an9ar.kappaweather.R
 import com.an9ar.kappaweather.theme.AppTheme
 
 @Composable
 fun SettingsScreen() {
-    Box(modifier = Modifier.fillMaxSize().background(AppTheme.colors.background)) {
+    Column(modifier = Modifier.fillMaxSize().background(AppTheme.colors.background)) {
+        Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                        .fillMaxSize()
+                        .weight(0.4f)
+        ) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Image(
+                        imageVector = vectorResource(id = R.drawable.ic_kappa_sign),
+                        colorFilter = ColorFilter.tint(AppTheme.colors.text),
+                        modifier = Modifier.preferredSize(64.dp)
+                )
+                Spacer(modifier = Modifier.preferredHeight(8.dp))
+                Text(text = "Version 1.0.0", color = AppTheme.colors.text)
+            }
+        }
+        Column(Modifier.background(AppTheme.colors.warning).fillMaxSize().weight(0.6f)) {
 
+        }
     }
 }
