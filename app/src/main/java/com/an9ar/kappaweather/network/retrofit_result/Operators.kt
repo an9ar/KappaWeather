1ec912fb6bec2722aa9d17bd004b62cc1e1ce297
Dmitriy <63235125+an9ar@users.noreply.github.com>
2021-01-12 10:38:51 +0300

    Added Resource wrapper for network calls

--- app/src/main/java/com/an9ar/kappaweather/network/retrofit_result/Operators.kt
+++ app/src/main/java/com/an9ar/kappaweather/network/retrofit_result/Operators.kt
@@ -23,6 +23,17 @@ fun <T> Result<T>.asFailure(): Result.Failure<*> {
     return this as Result.Failure<*>
 }
 
+fun <T, R> Result<T>.map(transform: (value: T) -> R): Result<R> {
+    return when(this) {
+        is Result.Success -> Result.Success.Value(transform(value))
+        is Result.Failure<*> -> this
+    }
+}
+
+fun <T, R> Result<T>.flatMap(transform: (result: Result<T>) -> Result<R>): Result<R> {
+    return transform(this)
+}
+
 fun <T> Result<T>.onResult(
     onSuccess: (Result<T>) -> Unit,
     onFailure: (Result<T>) -> Unit
