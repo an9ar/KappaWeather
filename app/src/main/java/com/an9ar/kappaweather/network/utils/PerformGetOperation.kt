040a5c0144926a404180ce264462e78aab91b9ef
Dmitriy <63235125+an9ar@users.noreply.github.com>
2021-01-18 17:43:46 +0300

    Added getOperation wrapper

--- app/src/main/java/com/an9ar/kappaweather/network/utils/PerformGetOperation.kt
+++ app/src/main/java/com/an9ar/kappaweather/network/utils/PerformGetOperation.kt
@@ -3,10 +3,9 @@ package com.an9ar.kappaweather.network.utils
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.liveData
 import androidx.lifecycle.map
-import com.an9ar.kappaweather.network.retrofit_result.Result
 import kotlinx.coroutines.Dispatchers
 
-fun <T> performGetOperation(
+fun <T> performFetchOperation(
     networkCall: suspend () -> Resource<T>,
     saveCallResult: suspend (T) -> Unit
 ): LiveData<Resource.Status> =
@@ -19,6 +18,25 @@ fun <T> performGetOperation(
             emit(Resource.Status.COMPLETED)
         } else if (responseStatus.status == Resource.Status.ERROR) {
             emit(Resource.Status.ERROR)
+        }
+    }
+
+fun <T, A> performGetOperation(
+    databaseQuery: () -> LiveData<T>,
+    networkCall: suspend () -> Resource<A>,
+    saveCallResult: suspend (A) -> Unit
+): LiveData<Resource<T>> =
+    liveData(Dispatchers.IO) {
+        emit(Resource.loading())
+        val source = databaseQuery.invoke().map { Resource.success(it) }
+
+        val responseStatus = networkCall.invoke()
+        if (responseStatus.status == Resource.Status.SUCCESS) {
+            saveCallResult(responseStatus.data!!)
+            emitSource(source)
+        } else if (responseStatus.status == Resource.Status.ERROR) {
+            emit(Resource.error(responseStatus.message!!))
+            emitSource(source)
         }
     }
 
