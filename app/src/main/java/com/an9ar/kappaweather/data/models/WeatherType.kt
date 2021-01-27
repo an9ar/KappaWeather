31e8eb48ed2ec95f99215a907b8b90ca87ee1757
Dmitriy <63235125+an9ar@users.noreply.github.com>
2021-01-28 00:59:19 +0300

    Added different weather types

---
+++ app/src/main/java/com/an9ar/kappaweather/data/models/WeatherType.kt
@@ -0,0 +1,15 @@
+package com.an9ar.kappaweather.data.models
+
+import com.an9ar.kappaweather.R
+
+sealed class WeatherType (iconId: Int) {
+    object ClearSky : WeatherType(iconId = R.drawable.ic_weather_clear_sky)
+    object FewClouds : WeatherType(iconId = R.drawable.ic_weather_few_clouds)
+    object ScatteredClouds : WeatherType(iconId = R.drawable.ic_weather_scattered_clouds)
+    object BrokenClouds : WeatherType(iconId = R.drawable.ic_weather_broken_clouds)
+    object ShowerRain : WeatherType(iconId = R.drawable.ic_weather_shower_rain)
+    object Rain : WeatherType(iconId = R.drawable.ic_weather_rain)
+    object Thunderstorm : WeatherType(iconId = R.drawable.ic_weather_thunderstorm)
+    object Snow : WeatherType(iconId = R.drawable.ic_weather_snow)
+    object Mist : WeatherType(iconId = R.drawable.ic_weather_mist)
+}
